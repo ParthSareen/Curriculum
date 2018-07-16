2ef8824443c3fee8d795bf19c3491d9c418437a6
Parth Sareen <parth0612@live.com>
2018-07-16 16:50:45 -0400

    Add files via upload

---
+++ adafruitio_16_servo/config.h
@@ -0,0 +1,24 @@
+/************************ Adafruit IO Config *******************************/
+
+// visit io.adafruit.com if you need to create an account,
+// or if you need your Adafruit IO key.
+#define IO_USERNAME    "ParthSareen"
+#define IO_KEY         "c0afd1d1b10c45d9b984319e78e58e30"
+
+/******************************* WIFI **************************************/
+
+// the AdafruitIO_WiFi client will work with the following boards:
+//   - HUZZAH ESP8266 Breakout -> https://www.adafruit.com/products/2471
+//   - Feather HUZZAH ESP8266 -> https://www.adafruit.com/products/2821
+//   - Feather M0 WiFi -> https://www.adafruit.com/products/3010
+//   - Feather WICED -> https://www.adafruit.com/products/3056
+
+#define WIFI_SSID       "sareenhome"
+#define WIFI_PASS       "parthprisha0610"
+
+// comment out the following two lines if you are using fona or ethernet
+#include "AdafruitIO_WiFi.h"
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+
+
+
