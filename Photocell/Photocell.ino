9c51795328bffd4d1a76123377141a257515ea16
Parth Sareen <parth0612@live.com>
2018-07-17 16:24:53 -0400

    Add files via upload

---
+++ Photocell/Photocell.ino
@@ -0,0 +1,33 @@
+int pRes = 0;
+int ledPin = 14;
+int value;
+
+void setup() {
+  pinMode (ledPin, OUTPUT);
+  pinMode (pRes, INPUT);
+  Serial.begin(115200);
+  }
+
+void loop() {
+  value = analogRead(pRes);
+  Serial.print(value);
+
+  if (value < 200){
+    digitalWrite(ledPin, LOW);
+    Serial.println(" Bright");
+  }
+  else if( value < 400) {
+    digitalWrite(ledPin, LOW);
+    Serial.println(" lightish");
+  }
+
+  else if (value > 400 ){
+    digitalWrite(ledPin, HIGH);
+    Serial.println(" Dark");
+    }
+     
+  }
+
+  
+
+
