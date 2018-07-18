ff02f8daa69e922305d4a31625613740e95ec024
Parth Sareen <parth0612@live.com>
2018-07-18 17:38:55 -0400

    Add files via upload

---
+++ flame/flame.ino
@@ -0,0 +1,24 @@
+const int flame = 0;
+const int buzz = 14;
+
+int value;
+
+void setup() {
+  pinMode (flame, INPUT); 
+  pinMode (buzz, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  value = analogRead(flame);
+  Serial.println(value);
+
+  if (value > 200 && value < 1024){
+    digitalWrite(buzz, HIGH);
+    }
+  else {
+    digitalWrite(buzz, LOW);
+    }
+  delay(1000);
+
+}
