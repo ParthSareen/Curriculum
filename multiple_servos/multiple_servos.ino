2ac5411cd0756f46b1f9c5b6bd9e187506d27805
Parth Sareen <parth0612@live.com>
2018-07-19 16:18:30 -0400

    Add files via upload

---
+++ multiple_servos/multiple_servos.ino
@@ -0,0 +1,31 @@
+#include <Servo.h> //Servo library
+
+//Define Servos
+
+Servo servo1;
+Servo servo2;
+
+// servo position in degrees
+int servoPos = 0;
+
+void setup() {
+// Define servo signal inputs 
+servo1.attach(12);
+servo2.attach(14);
+}
+
+void loop() {
+  //scan from 0 to 180 degrees
+
+  for (servoPos = 0; servoPos <180; servoPos ++){
+    servo1.write(servoPos);
+    servo2.write(servoPos);
+    delay(100);
+    }
+   //scan from 180 to 0 degrees
+   for(servoPos = 180; servoPos>0; servoPos--){
+      servo1.write(servoPos);
+      servo2.write(servoPos);
+      delay(100);
+    }
+  }
