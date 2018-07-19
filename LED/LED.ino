25b8d77f06f66e4c04489c24fdaa205a661d45d4
Parth Sareen <parth0612@live.com>
2018-07-19 15:44:52 -0400

    Add files via upload
    Comments originally done by Irfan, edited by Parth

--- LED/LED.ino
+++ LED/LED.ino
@@ -1,9 +1,17 @@
+/*
+ This program will allow you to light up an LED and allow it to blink(Not Sure??)
+ */
+
 void setup() {
+  //Initialize the pin 2 to output
   pinMode(2, OUTPUT);
 }
 
+
 void loop() {
+  //Gives the LOW value to Pin 2
   digitalWrite(2, LOW);
   delay(500);
+  //Gives the HIGH value to Pin 2
   digitalWrite(2, HIGH);
 }
